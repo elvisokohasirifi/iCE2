ac6f6db22a245c54368bf45b908e81d2c37400d5
El Vis <elvisokohasirifi@gmail.com>
2017-03-07 04:13:50 +0000

    Welcome screen added

--- app/src/main/java/com/example/sirelvis/ice/SignUp.java
+++ app/src/main/java/com/example/sirelvis/ice/SignUp.java
@@ -2,7 +2,9 @@ package com.example.sirelvis.ice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 /**
  * Created by Sir Elvis on 3/7/2017.
@@ -14,6 +16,10 @@ public class SignUp extends Activity{
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.signup);
         //final Button logIn = (Button) findViewById(R.id.logInBut);
+    }
 
+    public void signUp(View v){
+        Toast myToast = Toast.makeText(getApplicationContext(), "You will be able to sign up soon", Toast.LENGTH_LONG);
+        myToast.show();
     }
 }
